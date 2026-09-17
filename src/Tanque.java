2ec44809801d402b3a05cde8fad0499583fc9f0d
alespyba1 <aspindola22@alumnos.uaq.mx>
2026-09-17 09:56:30 -0600

    Actualizacdión estudiante A clase tanque

--- src/Tanque.java
+++ src/Tanque.java
@@ -6,6 +6,28 @@ public class Tanque {
     private String Id;
     private double capacidadMaxima;
     private double nivelActual;
+    private TankState estado;
+
+
+    public Tanque(String Id, double capacidadMaxima, double nivelInicial) {
+        this.Id = Id;
+        this.capacidadMaxima = capacidadMaxima;
+        this.estado = TankState.DETENIDO;
+
+        if (nivelInicial < 0) {
+            this.nivelActual = 0;
+        }
+
+        else if (nivelInicial > capacidadMaxima) {
+            this.nivelActual = capacidadMaxima;
+        }
+
+        else {
+            this.nivelActual = nivelInicial;
+        }
+    }
 }
+
+
 
 
