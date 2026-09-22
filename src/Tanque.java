1b15d8a94f42c7e04f8ae70423737e3ecaafeb15
alespyba1 <aspindola22@alumnos.uaq.mx>
2026-09-21 22:05:46 -0600

    Implementa atributos y constructor de Tanque

--- src/Tanque.java
+++ src/Tanque.java
@@ -1,33 +1,41 @@
-enum TankState{
-    LLENADO, VACIADO, DETENIDO;
+enum TankState {
+    LLENANDO, VACIANDO, DETENIDO;
 }
 
 public class Tanque {
-    private String Id;
+
+    private String id;
     private double capacidadMaxima;
     private double nivelActual;
     private TankState estado;
 
-
-    public Tanque(String Id, double capacidadMaxima, double nivelInicial) {
-        this.Id = Id;
+    public Tanque(String id, double capacidadMaxima, double nivelInicial) {
+        this.id = id;
         this.capacidadMaxima = capacidadMaxima;
         this.estado = TankState.DETENIDO;
 
         if (nivelInicial < 0) {
             this.nivelActual = 0;
-        }
-
-        else if (nivelInicial > capacidadMaxima) {
+        } else if (nivelInicial > capacidadMaxima) {
             this.nivelActual = capacidadMaxima;
-        }
-
-        else {
+        } else {
             this.nivelActual = nivelInicial;
         }
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public double getNivelActual() {
+        return nivelActual;
+    }
+
+    public TankState getEstado() {
+        return estado;
+    }
 }
-
-
-
-
